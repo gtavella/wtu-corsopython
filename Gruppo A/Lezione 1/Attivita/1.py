735d67d7cc0dc4db11f18e2d4cc61dafe156e4e2
Giuseppe Tavella <93888736+gtavella@users.noreply.github.com>
2024-02-19 07:01:48 +0100

    Update 1.py

--- Gruppo A/Lezione 1/Attivita/1.py
+++ Gruppo A/Lezione 1/Attivita/1.py
@@ -1,1 +1,9 @@
+# Scrivere una funzione che prende una stringa come input e restituisce la stessa stringa senza spazi bianchi.
 
+
+
+# Scrivere una funzione che conta quante volte una determinata lettera appare in una stringa.
+
+
+
+# Scrivi una funzione che prenda una stringa come input e restituisca il numero di vocali presenti nella stringa.
