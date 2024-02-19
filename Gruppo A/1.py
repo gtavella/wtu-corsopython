9174372a140fb6baee5cd3cbc2b233f6af14ae38
Giuseppe Tavella <93888736+gtavella@users.noreply.github.com>
2024-02-19 17:56:56 +0100

    Create 1.py

---
+++ Gruppo A/1.py
@@ -0,0 +1,86 @@
+
+
+# operatore di assegnamento: assegno il valore 5 alla variabile x. non ritorna niente
+x=5
+
+# operatore di equivalenza: risponde alla domanda, e' vero che la variabile x e' uguale al valore 5? ritorna un booleano
+# e' vero che e' vero che x e' uguale a 5?
+x==5
+
+# operatore diverso da: risponde alla domanda, e' vero che la variabile x e' diversa dal valore 5? ritorna un booleano
+# siccome x e' uguale a 5, e' vero che x e' diverso da 5? no, e' falso che x e' diverso da 5.
+# quindi e' vero che e' falso che x e' diverso da 5, cioe' e' vero che e' vero che x e' uguale a 5
+x!=5
+
+
+#*************
+
+
+# condizionali 
+
+nome='Giuseppe'
+if nome=='Giorgia':
+  print('Benvenuta Giorgia')
+elif nome=='Giuseppe':
+  print('Benvenuto Giuseppe')
+else:
+  print('Guarda la prossima volta')
+
+
+
+
+#****************
+
+
+# data una lista L, seleziona una sottolista R di L che parte dall'indice j e finisce all'indice n
+# detto anche "loop parametrico", molto frequente  negli esercizi d'esame
+# anche utilizzato per creare una copia dei valori di una lista
+L=['a', 'b', 'c', 'd']
+#  0     1    2    3     
+R=[]
+
+j=0
+n=len(L)
+for i in range(j,n):
+  R.append(L[i])
+
+
+
+#**************
+
+
+# salva il massimo tra due numeri
+# z contiene il piu' grande tra due numeri
+x=3
+y=1
+z=x
+
+if x>y:
+  z=x
+else:
+  z=y
+
+
+#**********
+
+# richiesta: verifica che una lista e' non crescente
+
+# parto dal definire cosa e' una lista crescente
+# una lista e' crescente quando l'elemento attuale e' strettamente minore del prossimo
+# L[i] < L[i+1]
+# esempio: 1,2,3,4,5,6..
+
+# poi nego la condizione di crescenza, quindi ottengo che una lista e' non crescente (quindi quello che mi interessa) 
+# quando o l'elemento attuale e' uguale al prossimo, o l'elemento attuale e' maggiore del prossimo
+# L[i] >= L[i+1]
+# esempio: 6,5,5,4,2,1
+# esempio: 6,4,3,2,1
+
+# quindi in una lista non crescente, tutti gli elementi devono essere in quest'ultimo modo.
+# di conseguenza, se anche solo due elementi sono crescenti, posso dire che la lista non e' non crescente
+
+
+#*************
+
+
+
